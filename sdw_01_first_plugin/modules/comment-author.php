5cc67dfdf78dd62b727c493384a209cb44db8862
LinusRichter <linus.35287@gmail.com>
2024-04-13 13:44:29 +0200

    fix: to many args

--- sdw_01_first_plugin/modules/comment-author.php
+++ sdw_01_first_plugin/modules/comment-author.php
@@ -4,14 +4,13 @@ namespace LinusRichter\FirstPlugin;
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-//add_filter('get_comment_author_url', [Changer::class, 'change_comment_author_url'], 10, 3);
-add_filter('get_comment_author_url', ['LinusRichter\FirstPlugin\Changer', 'change_comment_author_url'], 10, 3);
-add_filter('get_comment_author', ['LinusRichter\FirstPlugin\Changer', 'change_comment_author_name'], 10, 3);
+add_filter('get_comment_author_url', ['LinusRichter\FirstPlugin\Changer', 'change_comment_author_url'], 10, 0);
+add_filter('get_comment_author', ['LinusRichter\FirstPlugin\Changer', 'change_comment_author_name'], 10, 0);
 
 class Changer
 {
     /**
-     * Changes the comment-author link
+     * Changes the comment-author-link
      */
     public static function change_comment_author_url()
     {
