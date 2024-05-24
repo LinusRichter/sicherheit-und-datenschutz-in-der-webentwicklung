03c2e25fa01bcef8778394da1a564857fcc4f82f
LinusRichter <linus.35287@gmail.com>
2024-05-24 16:51:33 +0200

    fundament

---
+++ sdw_own_plugin/modules/classifier.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace LinusNiko\Own;
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+add_filter('wp_loaded',['LinusNiko\Own\Classifier', 'init'], 5);
+
+/**
+ * Database module for the THM Security plugin.
+ */
+class Classifier
+{
+    public static function init()
+    {
+        $request_class = 'normal';
+
+        if (preg_match('/\/wp-config.php/i', $_SERVER['REQUEST_URI']))
+        {
+            $request_class = 'config-grabber';
+        }
+
+        header("X-THMSEC: ENABLED");
+        header("X-THMSEC-CLASS: $request_class");
+
+        if ($request_class != 'normal')
+        {
+            header("HTTP/1.1 404 Not Found");
+            exit;
+        }
+    }
+}
--- sdw_own_plugin/sdw_own_plugin.php
+++ sdw_own_plugin/sdw_own_plugin.php
@@ -10,7 +10,8 @@ namespace LinusNiko\Own;
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-require_once(dirname(__FILE__) . '/modules/log.php');
-require_once(dirname(__FILE__) . '/modules/wp-scam.php');
+require_once(dirname(__FILE__) . '/modules/log.php'); //requires database.php
+require_once(dirname(__FILE__) . '/modules/username_protection.php');
+require_once(dirname(__FILE__) . '/modules/classifier.php');
 
 ?>
